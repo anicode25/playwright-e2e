516e3a3173b687bc8520b4aad3dc78a9c41ed413
Aniket Ghosh <cx330@codelogicx.link>
2025-07-29 16:23:01 +0530

    add: user register script

---
+++ tests/user_register.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.automationexercise.com/');
+  await expect(page).toHaveURL('https://www.automationexercise.com/');
+  await expect(page).toHaveTitle('Automation Exercise');
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+  if (testInfo.status !== testInfo.expectedStatus) {
+    await page.screenshot({ path: `screenshots/${testInfo.title}.png`, fullPage: true });
+  }
+});
+
+test('register a new user', async ({ page }) => {
+  await page.locator('//a[@href="/login"]').click();
+  await expect(page.getByRole('heading', { name: 'New User Signup!' })).toBeVisible();
+
+  await page.locator('[data-qa="signup-name"]').fill('Test Code');
+  await page.locator('[data-qa="signup-email"]').fill('testcode@code.com');
+  await page.locator('[data-qa="signup-button"]').click();
+
+  await expect(page.getByRole('heading', { name: 'Enter Account Information' })).toBeVisible();
+
+  await page.locator('[id="id_gender1"]').click();
+  await expect(page.locator('[id="id_gender1"]')).toBeChecked();
+  await page.locator('[data-qa="password"]').fill('Itobuz#1234');
+  await page.locator('[data-qa="days"]').selectOption('9');
+  await page.locator('[data-qa="months"]').selectOption('October');
+  await page.locator('[data-qa="years"]').selectOption('1994');
+  await page.locator('[data-qa="first_name"]').fill('Test');
+  await page.locator('[data-qa="last_name"]').fill('Code');
+  await page.locator('[data-qa="company"]').fill('XYZ');
+  await page.locator('[data-qa="address"]').fill('12, ABC Road');
+  await page.locator('[data-qa="state"]').fill('West Bengal');
+  await page.locator('[data-qa="city"]').fill('kolkata');
+  await page.locator('[data-qa="zipcode"]').fill('123456');
+  await page.locator('[data-qa="mobile_number"]').fill('0123456789');
+  await page.locator('[data-qa="create-account"]').click();
+
+  await expect(page.getByRole('heading', { name: 'Account Created!' })).toBeVisible();
+  await page.locator('[data-qa="continue-button"]').click();
+
+  await page.locator('[href="/delete_account"]').isVisible();
+  await page.locator('[href="/delete_account"]').click();
+  await expect(page.getByRole('heading', { name: 'Account Deleted!' })).toBeVisible();
+  await page.locator('[data-qa="continue-button"]').click();
+});
