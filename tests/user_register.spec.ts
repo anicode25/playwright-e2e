106a9b3c4ec03ae83407792beb91df5e1b89599b
Aniket Ghosh <cx330@codelogicx.com>
2025-07-31 14:02:32 +0530

    code refactor

--- tests/user_register.spec.ts
+++ tests/user_register.spec.ts
@@ -6,7 +6,7 @@ import { SignUp_Login_Page } from "../tests/pages/signup_login";
 const baseURL = process.env.BASE_URL!;
 const signUpName = faker.person.fullName();
 const signUpEmail = `testuser_${Date.now()}@mail.com`;
-const signUpPassword = 'Itobuz#1234'
+const signUpPassword = "Itobuz#1234";
 
 const home = new HomePage();
 const signupandlogin = new SignUp_Login_Page();
@@ -26,10 +26,7 @@ test.afterEach(async ({ page }, testInfo) => {
 
 test("register a new user", async ({ page }) => {
   await home.navigateToSignupPage(page);
-
   await signupandlogin.signUp(page, signUpName, signUpEmail);
-
   await signupandlogin.completeAccountInfo(page, signUpPassword);
-
   await signupandlogin.deleteAccount(page);
 });
