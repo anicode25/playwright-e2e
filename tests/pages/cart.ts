2e98540ab10ab308910c59fa5e8ccc21d676d0be
Aniket Ghosh <cx330@codelogicx.link>
2025-08-04 18:45:58 +0530

    verify product quantity in cart

---
+++ tests/pages/cart.ts
@@ -0,0 +1,27 @@
+import { Page, expect } from "@playwright/test";
+
+export class HomePage {
+  async goToHomePage(page: Page, baseURL: string) {
+    await page.goto(baseURL);
+    await expect(page).toHaveURL("https://www.automationexercise.com/");
+    await expect(page).toHaveTitle("Automation Exercise");
+  }
+
+  async navigateToSignupPage(page: Page) {
+    await page.locator('//a[@href="/login"]').click();
+    await expect(
+      page.getByRole("heading", { name: "New User Signup!" })
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole("heading", { name: "Login to your account" })
+    ).toBeVisible();
+  }
+
+  async navigateToProductsPage(page: Page) {
+    await page.locator('//a[@href="/products"]').click();
+    await expect(
+      page.getByRole("heading", { name: "All Products" })
+    ).toBeVisible();
+  }
+}
---
+++ tests/verify_product_quantity_in_cart.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../tests/pages/home";
+import { ProductsPage } from "../tests/pages/products";
+
+const baseURL = process.env.BASE_URL!;
+
+const home = new HomePage();
+const products = new ProductsPage();
+
+test.beforeEach(async ({ page }) => {
+  await home.goToHomePage(page, baseURL);
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+  if (testInfo.status !== testInfo.expectedStatus) {
+    await page.screenshot({
+      path: `screenshots/${testInfo.title}.png`,
+      fullPage: true,
+    });
+  }
+});
+
+test("Verify product quantity in cart", async ({ page }) => {
+  const productName = await page
+    .locator(".productinfo p")
+    .first()
+    .textContent();
+  await home.navigateToProductsPage(page);
+  await page.waitForTimeout(3000);
+  await products.verifyProductsList(page);
+
+  await products.verifyFistProductName(page);
+
+  await products.goToProductDetailFromList(page);
+  await products.verifyProductDetails(page, productName!);
+});
