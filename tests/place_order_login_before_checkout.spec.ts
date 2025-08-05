c06536918534dd8002fa687c0e250c7146ff872e
Aniket Ghosh <cx330@codelogicx.link>
2025-08-05 16:02:23 +0530

    added: place order:login before checkout

--- tests/place_order_login_before_checkout.spec.ts
+++ tests/place_order_login_before_checkout.spec.ts
@@ -3,12 +3,11 @@ import { HomePage } from "../tests/pages/home";
 import { SignUp_Login_Page } from "../tests/pages/signup_login";
 import { ProductsPage } from "../tests/pages/products";
 import { CartPage } from "../tests/pages/cart";
-import { faker } from "@faker-js/faker";
 
 const baseURL = process.env.BASE_URL!;
-const signUpName = faker.person.fullName();
-const signUpEmail = `testuser_${Date.now()}@mail.com`;
-const signUpPassword = "Itobuz#1234";
+const userName = process.env.REGISTER_USER_NAME!;
+const userEmail = process.env.REGISTER_USER_EMAIL!;
+const userPassword = process.env.REGISTER_USER_PASSWORD!;
 
 const home = new HomePage();
 const products = new ProductsPage();
@@ -28,7 +27,9 @@ test.afterEach(async ({ page }, testInfo) => {
   }
 });
 
-test("Place Order: Register while checkout", async ({ page }) => {
+test("Place Order: Login before checkout", async ({ page }) => {
+  await home.navigateToSignupPage(page);
+  await signupandlogin.logIn(page, userName, userEmail, userPassword);
   await home.navigateToProductsPage(page);
   await page.waitForTimeout(3000);
   await products.verifyProductsList(page);
@@ -37,16 +38,7 @@ test("Place Order: Register while checkout", async ({ page }) => {
   await products.addProductToCart(page, 2);
   await products.viewCartFromPopup(page);
   await page.waitForTimeout(500);
-  const checkoutButton = page.locator("a.btn.btn-default.check_out", {
-    hasText: "Proceed To Checkout",
-  });
-  await cart.proceedToCheckoutWithoutLogin(page);
-  await page.waitForTimeout(1000);
-  await signupandlogin.signUp(page, signUpName, signUpEmail);
-  await signupandlogin.completeAccountInfo(page, signUpPassword);
-
-  await page.locator('[href="/view_cart"]').nth(0).click();
   await cart.proceedToCheckoutWithLogin(page);
   await cart.completePaymentDetailsAndConfirmOrder(page);
-  await signupandlogin.deleteAccount(page);
+  // await signupandlogin.deleteAccount(page);   // Optionall
 });
